d03012b36f70d90d7f2b853c1596b12937220531
Kyle <kaledoux@gmail.com>
2021-05-06 16:39:42 -0400

    first commit

---
+++ public/javascripts/debounce.js
@@ -0,0 +1,9 @@
+function debounce(func, delay) {
+  let timeout;
+  return (...args) => {
+    if (timeout) { clearTimeout(timeout) }
+    timeout = setTimeout(() => func.apply(null, args), delay);
+  };
+};
+
+export {debounce}
